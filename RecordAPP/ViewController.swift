9940c638e1c877786e9d525afc2ae39f330fc1ff
离场悲剧 <bwang@BWANGdeMacBook-Pro.local>
2016-06-25 21:36:49 +0800

    测试APP的初始版本
    1.创建基本的APP框架
    2.创建静态测试文本
    3.创建两个测试按键
    4.创建动态TEXT框并用swift进行测试

--- RecordAPP/ViewController.swift
+++ RecordAPP/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tf.text = "V 1.0.1"     //版本信息
     }
 
     override func didReceiveMemoryWarning() {
