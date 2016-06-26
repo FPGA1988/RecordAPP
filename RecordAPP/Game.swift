bc76b582d8c010f56dfe926865b93ddc724f348c
离场悲剧 <bwang@bwangdemacbook-pro.local>
2016-06-26 21:16:20 +0800

    1.在ControlView添加游戏主界面背景 2.添加三个主界面选项 3.保留一个text以作后用 4.准备创建游戏界面

---
+++ RecordAPP/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  RecordAPP
+//
+//  Created by BWANG on 16/6/26.
+//  Copyright © 2016年 bwang. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class GameSceneViewControl: UIViewController {
+    
+}
